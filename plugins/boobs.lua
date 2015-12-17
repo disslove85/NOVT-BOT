6a9aa7683dcdee34c78819cf145b894bed9f1de2
disslove85 <m.disslove.m@gmail.com>
2015-12-17 20:44:34 +0330

    Update boobs.lua

--- plugins/boobs.lua
+++ plugins/boobs.lua
@@ -59,8 +59,8 @@ end
 return {
   description = "Gets a random boobs or butts pic", 
   usage = {
-    "!boobs: Get a boobs NSFW image. 🔞",
-    "!butts: Get a butts NSFW image. 🔞"
+    "!boobs: ممه. 🔞",
+    "!butts: کون. 🔞"
   },
   patterns = {
     "^!boobs$",
